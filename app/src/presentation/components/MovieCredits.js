b51b2768e80bae21fa39b40cfeb06e4b4db35b8d
tmaurie <thomas.maurier91@gmail.com>
2023-08-28 15:00:06 +0200

    added translations 1

--- app/src/presentation/components/MovieCredits.js
+++ app/src/presentation/components/MovieCredits.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { LanguageContext } from "App/src/presentation/context/LanguageContext";
+import dictionary from "App/src/languages/dictionary";
 
-const MovieCredits = (props) => {
+const MovieCredits = ({credits}) => {
+  const { language } = useContext(LanguageContext);
+  const translations = dictionary[language];
+
   return (<div className="movie-credits content-card">
-    <h3>Directed by</h3>
+    <h3>{translations.directed_by}</h3>
     <div className="crew">
-      {props.movieCredits.crew.filter((crew) => crew.job === "Director").map((director) => (
+      {credits.crew.filter((crew) => crew.job === "Director").map((director) => (
         <div className="director" key={director.id}>
           <span>{director.name}</span>
         </div>))}
 
     </div>
-    <h3>Cast</h3>
+    <h3>{translations.cast}</h3>
     <div className="cast">
-      {props.movieCredits.cast.slice(0, 8).map((actor) => (<div className="actor" key={actor.id}>
-        <span>{actor.name}</span> <span>as {actor.character}</span>
+      {credits.cast.slice(0, 8).map((actor) => (<div className="actor" key={actor.id}>
+        <span>{actor.name}</span> <span> {translations.as} {actor.character}</span>
       </div>))}
     </div>
 
