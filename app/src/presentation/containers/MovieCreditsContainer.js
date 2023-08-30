2e245d029f90fd89ace850cd044ff304b9e3339c
tmaurie <thomas.maurier91@gmail.com>
2023-08-30 23:31:42 +0200

    added translations

--- app/src/presentation/containers/MovieCreditsContainer.js
+++ app/src/presentation/containers/MovieCreditsContainer.js
@@ -1,31 +1,15 @@
-import React, { useEffect, useState } from "react";
-import fetchMovieCredits from "App/src/core/usecases/movieCredits";
-import { useParams } from "react-router-dom";
+import React from "react";
 import MovieCredits from "App/src/presentation/components/MovieCredits";
 
-const MovieCreditsContainer = () => {
-  const { id } = useParams();
-  const [movieCredits, setMovieCredits] = useState(null);
+const MovieCreditsContainer = ({ credits }) => {
 
-  useEffect(() => {
-    const fetchCredits = async () => {
-      try {
-        const credits = await fetchMovieCredits(id);
-        setMovieCredits(credits);
-      } catch (error) {
-        console.log("Failed to fetch movie credits:", error);
-      }
-    };
-
-    fetchCredits();
-  }, []);
-
-  if (!movieCredits) {
+  if (!credits) {
     return <div>Loading movie credits...</div>;
   }
+  console.log(credits);
 
   return (
-    <MovieCredits movieCredits={movieCredits} />
+    <MovieCredits movieCredits={credits} />
   );
 };
 
