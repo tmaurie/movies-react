cb249cdb5816e340a8df6c5bc07a37d9da003951
tmaurie <thomas.maurier91@gmail.com>
2023-09-02 15:22:05 +0200

    added translations

--- app/src/core/usecases/movieDetails.js
+++ app/src/core/usecases/movieDetails.js
@@ -1,8 +1,8 @@
 import movieRepository from "App/src/data/repositories/movieRepository";
 
-const fetchMovieDetails = async (movieId) => {
+const fetchMovieDetails = async (movieId, lang) => {
   try {
-    return await movieRepository.fetchMovieDetails(movieId);
+    return await movieRepository.fetchMovieDetails(movieId, lang);
   } catch (error) {
     throw new Error("Failed to fetch movie details from the TMDB API.");
   }
