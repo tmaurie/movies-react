7887868755f01edceb6589bb3dea6ff74f52626e
tmaurie <thomas.maurier91@gmail.com>
2023-09-04 18:11:21 +0200

    added translations

--- app/src/presentation/containers/MovieDetailsContainer.js
+++ app/src/presentation/containers/MovieDetailsContainer.js
@@ -1,17 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import fetchMovieDetails from "App/src/core/usecases/movieDetails";
 import { useParams } from "react-router-dom";
 import MovieDetails from "App/src/presentation/components/MovieDetails";
+import { LanguageContext } from "App/src/presentation/context/LanguageContext";
+import dictionary from "App/src/languages/dictionary";
 
 
 const MovieDetailsContainer = () => {
+  const { language } = useContext(LanguageContext);
+  const translations = dictionary[language];
   const { id } = useParams();
   const [movieDetails, setMovieDetails] = useState(null);
 
   useEffect(() => {
     const fetchDetails = async () => {
       try {
-        const details = await fetchMovieDetails(id);
+        const details = await fetchMovieDetails(id, translations.lang_param);
         setMovieDetails(details);
       } catch (error) {
         console.log("Failed to fetch movie details:", error);
@@ -19,7 +23,7 @@ const MovieDetailsContainer = () => {
     };
 
     fetchDetails();
-  }, []);
+  }, [ id, language]);
 
   if (!movieDetails) {
     return <div>Loading movie details...</div>;
